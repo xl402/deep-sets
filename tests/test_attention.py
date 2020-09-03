c04a07aba481ff1cb6eaebb7e778c7dd4e04d7c8
xl402 <xl402@cam.ac.uk>
2020-09-04 00:07:08 +0100

    test scaled product attention constants modified

--- tests/test_attention.py
+++ tests/test_attention.py
@@ -6,24 +6,24 @@ from models import attention as attn
 
 def test_scaled_dot_product_attention_without_masked_input():
     queries = tf.constant([[0., 10., 0.]])
-    keys = tf.constant([[10., 0., 0.],
+    keys = tf.constant([[3., 0., 0.],
                         [0., 10., 0.],
-                        [0., 0., 10.],
-                        [0., 0., 10.]])
-    values = tf.constant([[1., 0.], [10., 0.], [100., 5.], [1000., 6.]])
+                        [0., 0., 4.],
+                        [0., 0., 1.]])
+    values = tf.constant([[5., 9.], [2., 6.], [5., 3.], [5., 3.]])
     output = attn.scaled_dot_product_attention(queries, keys, values, None)
-    expected = np.array([[10., 0.]])
+    expected = np.array([[2., 6.]])
     assert np.allclose(output.numpy(), expected)
 
 
 def test_scaled_dot_product_attention_with_masked_input():
     queries = tf.constant([[0., 10., 0.]])
-    keys = tf.constant([[10., 0., 0.],
+    keys = tf.constant([[3., 0., 0.],
                         [0., 10., 0.],
-                        [0., 0., 10.],
+                        [0., 0., 4.],
                         [0., 0., 0.]])
-    values = tf.constant([[1., 0.], [10., 0.], [100., 5.], [0., 0.]])
+    values = tf.constant([[5., 9.], [2., 6.], [5., 3.], [0., 0.]])
     mask = np.array([[0, 0, 0, 1]])
     output = attn.scaled_dot_product_attention(queries, keys, values, mask)
-    expected = np.array([[10., 0.]])
+    expected = np.array([[2., 6.]])
     assert np.allclose(output.numpy(), expected)
