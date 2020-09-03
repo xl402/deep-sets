7242bf594fdee091eb8e28a22cbb3b568730ec94
xl402 <xl402@cam.ac.uk>
2020-09-03 23:57:54 +0100

    test scaled dot product attention with and witout mask added

--- tests/test_attention.py
+++ tests/test_attention.py
@@ -4,13 +4,26 @@ import numpy as np
 from models import attention as attn
 
 
-def test_scaled_dot_product_attention():
+def test_scaled_dot_product_attention_without_masked_input():
+    queries = tf.constant([[0., 10., 0.]])
     keys = tf.constant([[10., 0., 0.],
                         [0., 10., 0.],
                         [0., 0., 10.],
                         [0., 0., 10.]])
     values = tf.constant([[1., 0.], [10., 0.], [100., 5.], [1000., 6.]])
-    queries = tf.constant([[0., 10., 0.]])
     output = attn.scaled_dot_product_attention(queries, keys, values, None)
+    expected = np.array([[10., 0.]])
+    assert np.allclose(output.numpy(), expected)
+
+
+def test_scaled_dot_product_attention_with_masked_input():
+    queries = tf.constant([[0., 10., 0.]])
+    keys = tf.constant([[10., 0., 0.],
+                        [0., 10., 0.],
+                        [0., 0., 10.],
+                        [0., 0., 0.]])
+    values = tf.constant([[1., 0.], [10., 0.], [100., 5.], [0., 0.]])
+    mask = np.array([[0, 0, 0, 1]])
+    output = attn.scaled_dot_product_attention(queries, keys, values, mask)
     expected = np.array([[10., 0.]])
     assert np.allclose(output.numpy(), expected)
