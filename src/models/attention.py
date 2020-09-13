2412749d72368b48926bf3147a8130ea6043e560
xl402 <xl402@cam.ac.uk>
2020-09-13 17:06:19 +0100

    multihead attention module added

--- src/models/attention.py
+++ src/models/attention.py
@@ -1,18 +1,61 @@
 import tensorflow as tf
+from tensorflow.keras.layers import Dense
 
 
 def scaled_dot_product_attention(queries, keys, values, mask):
     """
-    attention(q, k, v) = softmax(q @ k^T / sqrt(dim(k))) @ v
+    attention(q, keys, v) = softmax(q @ k^T / sqrt(dim(k))) @ v
     """
-    product = queries @ tf.transpose(keys)
+    product = tf.matmul(queries, keys, transpose_b=True)
     keys_dim = tf.cast(tf.shape(keys)[-1], tf.float32)
     scaled_product = product / tf.math.sqrt(keys_dim)
 
     if mask is not None:
         scaled_product += (mask * -1e9)
 
-    numerator = tf.nn.softmax(scaled_product, axis=-1)
-    attention = numerator @ values
+    attention_weights = tf.nn.softmax(scaled_product, axis=-1)
+    attention = tf.matmul(attention_weights, values)
 
     return attention
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, input_dim, num_heads, **kwargs):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.input_dim = input_dim
+
+        assert input_dim % self.num_heads == 0, "invalid number of heads"
+
+        self.depth = input_dim // self.num_heads
+
+        self.query_matrix = Dense(input_dim, **kwargs)
+        self.key_matrix = Dense(input_dim, **kwargs)
+        self.value_matrix = Dense(input_dim, **kwargs)
+
+        self.dense_out = Dense(input_dim)
+
+    def split_heads(self, x, batch_size):
+        output_shape = (batch_size, -1, self.num_heads, self.depth)
+        x = tf.reshape(x, output_shape)
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, queries, keys, values, mask=None):
+        batch_size = tf.shape(queries)[0]
+
+        queries = self.query_matrix(queries)
+        keys = self.key_matrix(keys)
+        values = self.value_matrix(values)
+
+        queries = self.split_heads(queries, batch_size)
+        keys = self.split_heads(keys, batch_size)
+        values = self.split_heads(values, batch_size)
+
+        attention = scaled_dot_product_attention(queries, keys, values, mask)
+        attention = tf.transpose(attention, perm=[0, 2, 1, 3])
+
+        output_shape = (batch_size, -1, self.input_dim)
+        concat_attention = tf.reshape(attention, output_shape)
+        output = self.dense_out(concat_attention)
+
+        return output
--- tests/test_attention.py
+++ tests/test_attention.py
@@ -1,7 +1,13 @@
-import tensorflow as tf
+from mock import patch
+
 import numpy as np
+import tensorflow as tf
+import pytest
 
 from models import attention as attn
+
+
+MULTIHEAD_INPUT_SHAPES = [(12, 5), (3, 4), (512, 7)]
 
 
 def test_scaled_dot_product_attention_without_masked_input():
@@ -27,3 +33,27 @@ def test_scaled_dot_product_attention_with_masked_input():
     output = attn.scaled_dot_product_attention(queries, keys, values, mask)
     expected = np.array([[2., 6.]])
     assert np.allclose(output.numpy(), expected)
+
+
+@pytest.mark.parametrize('shape', MULTIHEAD_INPUT_SHAPES)
+def test_raises_for_invalid_number_of_heads_for_multihead_attention(shape):
+    with pytest.raises(AssertionError) as e:
+        _ = attn.MultiHeadAttention(*shape)
+    assert 'invalid number of heads' in str(e)
+
+
+@patch('models.attention.Dense')
+def test_multihead_attention_without_masked_input(Dense):
+    Dense.return_value = tf.keras.layers.Dense(3, kernel_initializer='ones')
+    query = tf.constant([[0, 0., 0.],
+                         [0., 1., 0.],
+                         [0., 1., 1.],
+                         [1., 1., 1.]])
+    mha = attn.MultiHeadAttention(3, 3)
+    expected = np.array([[0, 0., 0.],
+                         [3., 3., 3.],
+                         [6., 6., 6.],
+                         [9., 9., 9.]])
+    output = mha(query, query, query)
+    output = output.numpy().reshape(expected.shape)
+    assert np.allclose(output, expected)
